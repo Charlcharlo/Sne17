28bb5197efd89740a2b0b842a7b64d771d731111
Charlcharlo <Charlcharlo@gmail.com>
2023-08-05 15:27:28 +0200

    Changes to vite config file

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Sne17/",
+  base: "https://charlcharlo.github.io/Sne17/",
   plugins: [react()],
 });
