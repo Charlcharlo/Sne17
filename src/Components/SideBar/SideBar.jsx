af1ea89edbbb2ca04d301460b11fc0a3bd9c9903
Charlcharlo <Charlcharlo@gmail.com>
2023-08-16 15:30:34 +0200

    Add Sidebar

---
+++ src/Components/SideBar/SideBar.jsx
@@ -0,0 +1,31 @@
+import LangButtons from "../LangButtons/LangButtons";
+import PdfDownloader from "../PdfDownloader/PdfDownloader";
+import "./SideBar.css";
+
+export default function SideBar({ hidden }) {
+  return (
+    <div className={`side-bar ${hidden && "hidden"}`}>
+      <div className="panel-section">
+        <h2 className="panel-section-header">Language</h2>
+        <LangButtons />
+      </div>
+      <div className="panel-section">
+        <h2 className="panel-section-header">Learn More</h2>
+        <a className="panel-link" href="">
+          About the author
+        </a>
+        <a className="panel-link" href="">
+          Ethekweni Strathub
+        </a>
+        <a className="panel-link" href="">
+          Ethekweni Municipality
+        </a>
+      </div>
+      <div className="panel-section">
+        <PdfDownloader />
+      </div>
+    </div>
+  );
+}
+
+SideBar.propTypes;
