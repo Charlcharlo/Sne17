0b13fbfa1c58cabb313cbc36fe4e5fd2c2eb9acd
Charlcharlo <Charlcharlo@gmail.com>
2024-05-02 22:28:38 +0200

    Configure for OpenGraph

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://charlcharlo.github.io/Sne17/",
+  base: "https://sne17.com",
   plugins: [react()],
 });
